e1ad385ef3d2fb5eda1516b632dd6db3f92e2ffc
Renato Angelo <renatoangelo@gmail.com>
2023-06-05 20:16:41 -0300

    Git Ignore

--- admin/include/config.php
+++ admin/include/config.php
@@ -5,7 +5,9 @@ define('HOST', 'localhost');
 define('DBNAME', 'frotasystem');
 define('PORT', '3306');
 
+
 // Endereço do projeto
 define('URL', 'http://localhost/frotasystem/');
 define('URLADMIN', 'http://localhost/frotasystem/admin/');
+
 ?>
